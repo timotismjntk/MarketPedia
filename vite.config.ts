e00b53df864ff7d1614f63bd0da085ecfbd8a3ee
timotismjntk <timotiussimanjuntak24@gmail.com>
2025-05-11 19:48:47 +0700

    ddsasad

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/MarketPedia/',  // Make sure this matches your GitHub repo name
   plugins: [
     react(),
     mode === 'development' &&
